f03bc3e5be371e7504eaefb7a83eb32a2bc38cf6
leomoko <leo.moko@imqs.co.za>
2021-08-25 10:18:29 +0200

    Dev Complete

--- app.ts
+++ app.ts
@@ -1,106 +1,155 @@
-let recordCount : number ,startIndex : number , endIndex : number ; 
-let columnHeaders : any [];
+let recordCount: number, startIndex: number, endIndex: number;
+let columnHeaders: any[];
 let resizeTimer: number;
 
-window.onload =  () => GetRecordCount();
+window.onload = () => getRecordCount();
 
-//Calculating how many records to display based on widow size
-function calRowPerView(){
-    return Math.floor((window.innerHeight - 250 )/ 70); 
+//calculating how many records to display based on widow size
+function calRowPerView() {
+  let x = Math.floor((window.innerHeight - 250) / 39);
+  if (x < 0) {
+    alert("Window screen to small to display records , Resize window");
+  }
+  return x;
 }
 
-function loadGrid(start: number){
-    startIndex = start;
-    endIndex = start + calRowPerView();
-    
-    //adding heading to the page header
-    document.getElementById("tableHeading").innerHTML = 'Onboarding Project Data Grid';
-    //clearing current table
-    document.getElementById("divTable").innerHTML = '';
-    //calling Records
-    GetRecords(startIndex ,endIndex); 
+function loadGrid(start: number) {
+  startIndex = start;
+  endIndex = start + calRowPerView();
 
+  if (startIndex < 0) {
+    startIndex = 0;
+    endIndex = calRowPerView();
+  }
+  if (endIndex >= recordCount) {
+    endIndex = recordCount - 1;
+  }
+  if (startIndex >= recordCount) {
+    startIndex = recordCount - calRowPerView();
+  }
+  if (startIndex === recordCount - 1) {
+    startIndex = startIndex - calRowPerView();
+  }
+  //creating header and search input field
+  document.getElementById("header").innerHTML = `<input type="number" 
+    id="search" onfocusout="Search(event)" placeholder="Search : Enter ID">`;
+
+  //clearing current table
+  document.getElementById("divTable").innerHTML = "";
+  //calling Records
+  getRecords(startIndex, endIndex);
 }
 
-function GetRecordCount(){
-    //calling record count from the api, full url http://localhost:2050/recordcount
-    $.get("/recordCount", function(numberRecords){
-        recordCount = numberRecords;
-        GetColumnHeaders(); 
-     }).fail(function(){
-         alert("Error while calling Record Count")
-     });
+function getRecordCount() {
+  //calling record count from the api, full url http://localhost:2050/recordcount
+  $.get("/recordCount", function (numberRecords) {
+    recordCount = numberRecords;
+    getColumnHeaders();
+  }).fail(function () {
+    alert("Error while calling Record Count");
+  });
 }
 
-function GetColumnHeaders(){
-    //calling column header from the api, full url http://localhost:2050/columns 
-    $.get("/columns", function(headers){
-        columnHeaders = JSON.parse(headers);
-        loadGrid(0);
-    }).fail(function(){
-        alert("Error while calling column header")
-    });
+function getColumnHeaders() {
+  //calling column header from the api, full url http://localhost:2050/columns
+  $.get("/columns", function (headers) {
+    columnHeaders = JSON.parse(headers);
+    loadGrid(0);
+  }).fail(function () {
+    alert("Error while calling column header");
+  });
 }
 
-function GetRecords(start: number , end: number ){
-    //calling records from the api, full url http://localhost:2050/records?from=1&to=3
-    $.get("/records?from="+ start + "&to=" + end, function(rec){
-       //creating table grid with records returned by the api
-        createTable(JSON.parse(rec));
-       }).fail(function(){
-           alert("Error while calling records")
-       });
+function getRecords(start: number, end: number) {
+  //calling records from the api, full url http://localhost:2050/records?from=1&to=3
+  $.get("/records?from=" + start + "&to=" + end, function (rec) {
+    //creating table grid with records returned by the api
+    createTable(JSON.parse(rec));
+  }).fail(function () {
+    alert("Couldn't load records , page will refresh");
+  });
 }
 
-//building table 
-function createTable(records: any[]){
-    let table = document.createElement("table");
-    let tableBody = document.createElement("tbody");
-    let tableHead = document.createElement("thead");
-    
-    //creating table columns 
-    for (let c = 0 ; c < columnHeaders.length; c++){
-        tableHead.appendChild(document.createElement("th"))
-                 .appendChild(document.createTextNode(columnHeaders[c]));
+//building table
+function createTable(records: any[]) {
+  let table = document.createElement("table");
+  // let table = this.$el("table");
+  let tableBody = document.createElement("tbody");
+  let tableHead = document.createElement("thead");
+
+  //creating table columns
+  for (let c = 0; c < columnHeaders.length; c++) {
+    tableHead
+      .appendChild(document.createElement("th"))
+      .appendChild(document.createTextNode(columnHeaders[c]));
+  }
+  table.appendChild(tableHead);
+
+  //creating table rows
+  for (let a = 0; a < records.length; a++) {
+    let tableRow = document.createElement("tr");
+    table.appendChild(tableRow);
+
+    let tdArray = records[a].length;
+
+    for (let b = 0; b < tdArray; b++) {
+      tableRow
+        .appendChild(document.createElement("td"))
+        .appendChild(document.createTextNode(records[a][b]));
     }
-    table.appendChild(tableHead);
-
-    //creating table rows
-    for (let a = 0 ; a < records.length; a++){
-        let tableRow = document.createElement("tr");
-        table.appendChild(tableRow);
-
-        let tdArray = records[a].length;
-
-        for (let b = 0; b < tdArray; b++){
-         tableRow.appendChild(document.createElement("td"))
-         .appendChild(document.createTextNode(records[a][b]));   
-        }
-    }
-    //Adding table to table div ID = divTable 
-    document.getElementById("divTable").appendChild(table)
-    //Adding buttons to to the footer tag
-    document.getElementById("footer").innerHTML = '<button onclick="BackButton()">Back</button> <button onclick="NextButton()">Next</button>';
+  }
+  //Adding table to table div ID = divTable
+  document.getElementById("divTable").appendChild(table);
+  //Adding buttons to to the footer tag
+  document.getElementById(
+    "footer"
+  ).innerHTML = `<button onclick="backButton()">Back</button> 
+    <button onclick="nextButton()">Next</button>`;
 }
 
-//Next button function
-function NextButton(){
-    if (endIndex < recordCount ) {
-       loadGrid(endIndex + 1)
-    }
+function Search(e) {
+  let inputValue = (<HTMLInputElement>document.getElementById("search")).value;
+  // let $table = $("table tr").toArray();
+
+  //search for record on current record view
+  // if (parseInt(inputValue) >= startIndex &&  parseInt(inputValue) <= endIndex){
+  //     for (let record of $table) {
+  //         let td = record.getElementsByTagName("td")[0];
+  //          if (td) {
+  //              let txtValue = td.textContent || td.innerText;
+  //              record.style.display =  txtValue.indexOf(inputValue)  > -1 ? "" : "none";
+  //          }
+  //      }
+  //     }else {
+  if (inputValue === "") {
+    inputValue = startIndex.toString();
+    loadGrid(parseInt(inputValue));
+  } else {
+    loadGrid(parseInt(inputValue));
+  }
+  // }
+}
+
+//next button function
+function nextButton() {
+  if (endIndex < recordCount - 1) {
+    loadGrid(endIndex + 1);
+  } else {
+    alert("No more records to load");
+  }
 }
 
 //back button function
-function BackButton(){
-    startIndex -= calRowPerView(); 
-    loadGrid(startIndex - 1);    
+function backButton() {
+  startIndex -= calRowPerView();
+  loadGrid(startIndex - 1);
 }
 
 //update table gride size based on current window size
 window.onresize = () => {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function (){  
-        calRowPerView();
-        loadGrid(startIndex);
-    },200);
-}
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function () {
+    calRowPerView();
+    loadGrid(startIndex);
+  }, 200);
+};
